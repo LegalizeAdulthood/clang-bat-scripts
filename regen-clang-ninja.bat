6e35140f445dc8bc553e4686ca3d200e37f5cbb4
Richard <legalize@xmission.com>
2024-03-15 12:27:00 -0600

    Replace regeneration scripts with presets

--- regen-clang-ninja.bat
+++
@@ -1,7 +0,0 @@
-set enable_sphinx=""
-if not "%SPHINX%"=="" set "enable_sphinx=-DLLVM_ENABLE_SPHINX_BOOL=ON -DSPHINX_EXECUTABLE:FILEPATH=%SPHINX%"
-cmake -G "Ninja Multi-Config" ^
--DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra ^
--DLLVM_TARGETS_TO_BUILD=X86 ^
--DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON ^
-%enable_sphinx% ../llvm %*
