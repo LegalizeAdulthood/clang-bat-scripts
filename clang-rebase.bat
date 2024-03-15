29082869c231a67a5192e6f6d6117d577af8b15f
Richard <legalize@xmission.com>
2024-03-15 12:24:32 -0600

    Improve error handling during rebasing

--- clang-rebase.bat
+++ clang-rebase.bat
@@ -1,4 +1,12 @@
 git checkout %1
 git restore .
 git rebase main
-if errorlevel 1 (pause && git rebase --continue)
+if errorlevel 1 goto :again
+exit /b 0
+
+:again
+pause
+git rebase --continue
+if errorlevel 1 goto :again
+exit /b 0
+
