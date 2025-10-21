2781d7211db0b415f3dc4c95b4bff49d90b34604
Richard <legalize@xmission.com>
2025-10-21 11:12:42 -0600

    Use preset

--- clang-update.bat
+++ clang-update.bat
@@ -1,5 +1,5 @@
 call clang-rebase-all 
 if errorlevel 1 goto :eof
 git checkout main
-cmake --build build --config Release --target clang-tidy %*
-cmake --build build --config Debug --target clang-tidy %*
+cd llvm
+cmake --workflow --preset tidy
